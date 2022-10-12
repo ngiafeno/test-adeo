48d2a59e519e04bd62f53a25ea4c026212b377f6
Fenoa <feno.a@celaneo.com>
2022-10-12 17:23:56 +0200

    test on empty filter result fails

--- test/combined.test.js
+++ test/combined.test.js
@@ -4,4 +4,8 @@ describe('Combine filter and count args', () => {
   it('Should filter the result to only matching animal  name (ry) and count only one', () => {
     expect(count(filter('ry'))).toEqual('[{"name":"Uzuzozne [1]","people":[{"name":"Lillie Abbott [1]","animals":[{"name":"John Dory"}]}]},{"name":"Satanwi [1]","people":[{"name":"Anthony Bruno [1]","animals":[{"name":"Oryx"}]}]}]');
   });
+  
+  it("Should return message nothing is found it the filter return nothing", () => {
+    expect(count(filter(''))).toEqual('Nothing found');
+  })
 });
